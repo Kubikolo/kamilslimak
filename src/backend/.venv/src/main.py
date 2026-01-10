6c2f04832ddc2a85bc7b09b3d6fc1d5c28f6a7a9
PiotrPich2024 <ppich@student.agh.edu.pl>
2026-01-10 18:42:04 +0100

    backend ready to test

--- src/backend/.venv/src/main.py
+++ src/backend/.venv/src/main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import pyrebase
+from uuid import uuid4
 
 load_dotenv()
 
@@ -67,7 +68,8 @@ def create_business(name, email, password):
         return None
 
 
-def create_offer(business_id, offer_id, name, description, price, image, add_points, cost_points):
+def create_offer(business_id, name, description, price, image, add_points, cost_points):
+    offer_id = uuid4()
     db.child("businesses").child(business_id).child("offers").child(offer_id).set({
         "name": name,
         "description": description,
@@ -77,15 +79,39 @@ def create_offer(business_id, offer_id, name, description, price, image, add_poi
         "cost_points": cost_points
     })
     print(f"Oferta {name} dodana do biznesu {business_id}")
-
+    return offer_id
 
 ##########
 # READ
 ##########
 
+def read_login(email, password):
+    try:
+        user = auth.sign_in_with_email_and_password(email, password)
+        uid = user['localId']
+        print(f"Zalogowano użytkownika {email} z UID: {uid}")
+        return uid
+    except Exception as e:
+        print("Błąd logowania:", e)
+        return None
+
+def read_client_info(client_id):
+    refrence = db.child("users").child(client_id)
+    if not refrence.get().val():
+        print("Nie znaleziono użytkownika o podanym id: ", client_id)
+        return
+    return refrence.get().val()
+
+def read_bussines_info(bussines_id):
+    refrence = db.child("businesses").child(bussines_id)
+    if not refrence.get().val():
+        print("nie znaleziono biznesu o id: ", bussines_id)
+        return
+
+    return refrence.get().val()
 
 def read_client_points_list(client_id):
-    temp_list = db.child("users").child(client_id).child("points").get().val()
+    temp_list = db.child("users").child(client_id).child("points").get().val() or dict()
     return_list = dict()
     for business_uid in temp_list:
         business_name = db.child("businesses").child(business_uid).child("name").get().val()
@@ -95,7 +121,11 @@ def read_client_points_list(client_id):
 
 
 def read_business_offer_info(bussines_id):
-    return db.child("businesses").child(bussines_id).child("offers").get().val()
+    refrence = db.child("businesses").child(bussines_id).child("offers")
+    if not refrence.get().val():
+        print("nie znaleziono ofert dla biznesu o id: ", bussines_id)
+        return
+    return refrence.get().val()
 
 
 ##########
@@ -137,10 +167,10 @@ def add_points(client_id, busines_id, offer_id):
 
     point_number = offer.get("add_points", 0)
 
-    curr_points = db.child("users").child(client_id).child("points").child(business_id).get().val() or 0
+    curr_points = db.child("users").child(client_id).child("points").child(busines_id).get().val() or 0
     curr_points += point_number
-    db.child("users").child(client_id).child("points").update({business_id: curr_points})
-    print(f"Dodano {point_number} punktów dla klienta {client_id} w biznesie {business_id}")
+    db.child("users").child(client_id).child("points").update({busines_id: curr_points})
+    print(f"Dodano {point_number} punktów dla klienta {client_id} w biznesie {busines_id}")
 
 
 ############
@@ -149,25 +179,29 @@ def add_points(client_id, busines_id, offer_id):
 
 if __name__ == '__main__':
     pass
-    # client_uid = create_user("jan@example.com", "supersecret123", "Janek")
-    #
-    # business_uid = create_business("Sklep Janek", "sklep@example.com", "biznes123")
-    #
-    # offer_id = "oferta001"
-    # create_offer(
-    #     business_id=business_uid,
-    #     offer_id=offer_id,
-    #     name="Pizza Margherita",
-    #     description="Pyszna pizza z mozzarellą i sosem pomidorowym",
-    #     price=25.0,
-    #     image="https://example.com/pizza.jpg",
-    #     add_points=10,
-    #     cost_points=100
-    # )
-    #
-    # add_points(client_uid, business_uid, offer_id)
-    #
-    #
-    # client_points = db.child("users").child(client_uid).child("points").child(business_uid).get().val()
-    # print(f"Aktualna liczba punktów klienta {client_uid} w biznesie {business_uid}: {client_points}")
-    print(read_client_points_list("fR0zS3zHbBUA6AUpx7fFeR4RvoA3"))
+    client_uid = create_user(
+        "ewa@example.com",
+        "tajne123",
+        "Ewa"
+    )
+
+    business_uid = create_business(
+        "Kawiarnia Ewa",
+        "kawiarnia@example.com",
+        "biznes123"
+    )
+
+    offer_id = create_offer(
+        business_id=business_uid,
+        name="Kawa Latte",
+        description="Latte z mlekiem owsianym",
+        price=16.0,
+        image="https://example.com/latte.jpg",
+        add_points=5,
+        cost_points=50
+    )
+
+    add_points(client_uid, business_uid, offer_id)
+
+    print(read_client_points_list(client_uid))
+
