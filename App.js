989a70d9e23f091a76d288e7c5b1626693fce5e4
WIKO0 <wiktor.kurek@interia.eu>
2026-01-10 16:13:47 +0100

    Revert "Merge branch 'main' of https://github.com/Kubikolo/kamilslimak"
    This reverts commit 202e7f600f95b07fc65e9418d9d7f9c27d56f722, reversing
    changes made to c20bef50255393041a5d6d9215d377e1c4852f1b.
    
    revert

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { style } from "./src/frontend/styles/styles";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import BottomTabs from './src/frontend/navigation/BottomTabs';
 
 export default function App() {
   return (
-    
+    <NavigationContainer>
+      <BottomTabs/>
+    </NavigationContainer>
   );
 }
