c0bdae744e64f5d0ebf1ad7433cfbc665797fedc
PiotrPich2024 <ppich@student.agh.edu.pl>
2026-01-10 22:27:46 +0100

    prosze dzialaj git bez git psikus pls

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomTabs from './src/frontend/navigation/BottomTabs';
-<<<<<<< HEAD
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createStaticNavigation } from '@react-navigation/native';
 import SignUpScreen from './src/frontend/screens/SignUpScreen';
@@ -36,15 +35,4 @@ const Navigation = createStaticNavigation(Stack);
 
 export default function App() {
   return (<Navigation/>);
-=======
-import ProfileScreen from './src/frontend/screens/ProfileScreen';
-
-export default function App() {
-  
-  return (
-    <NavigationContainer>
-      <ProfileScreen/>
-    </NavigationContainer>
-  );
->>>>>>> PPBACKEND
 }
