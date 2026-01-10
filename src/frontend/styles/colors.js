c20bef50255393041a5d6d9215d377e1c4852f1b
Kubikolo <jakub.kolotylo@gmail.com>
2026-01-10 15:54:03 +0100

    add styles

---
+++ src/frontend/styles/colors.js
@@ -0,0 +1,3 @@
+export const StyleColors = {
+    accent: "#FF6961"
+};
--- src/frontend/styles/styles.js
+++ src/frontend/styles/styles.js
@@ -1,5 +1,38 @@
 import { StyleSheet } from "react-native";
+import { StyleColors } from "./colors";
 
 export const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+    container: { 
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center' 
+    },
+
+    profileContainer: {
+        flex: 1,
+    },
+
+    profileHeaderText: {
+        fontSize: 30,
+        fontWeight: 800
+    },
+
+    profileHeader: {
+        height: 80,
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 16,
+        justifyContent: 'space-between'
+    },
+
+    profileBody: {
+        padding: 16,
+    },
+
+    profileButton: {
+        backgroundColor: StyleColors.accent,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
 });
