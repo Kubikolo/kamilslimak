50b8989736c829bfa76c79295ec29bec9817b962
LeoonKameleon <kacperwachala0@gmail.com>
2026-01-10 16:47:30 +0100

    qr screen prototype

--- src/frontend/screens/QRScreen.js
+++ src/frontend/screens/QRScreen.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { styles } from '../styles/styles.js';
+import QRCode from 'react-native-qrcode-svg';
 
 export default function QRScreen() {
   return (
     <View style={styles.qrContainer}>
-      <Text style={styles.qrHeader}>Twój Kod QR</Text>
+        <View style={styles.qrHeader}>
+            <Text style={styles.qrHeaderText}>Twój Kod QR</Text>
+        </View>
+        <View style={styles.qrCode}>
+            <QRCode size={200} value='AdLDK1lt1kIidZWJdk7Y'></QRCode>
+        </View>
+        <View style={styles.qrBody}>
+            <Text style={styles.qrText}>Zeskanuj powyższy kod u sprzedawcy, aby otrzymać jego punkty!</Text>
+        </View>
     </View>
   );
 }
--- src/frontend/styles/styles.js
+++ src/frontend/styles/styles.js
@@ -1,5 +1,42 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+    container: { 
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center' 
+    },
+
+    qrContainer: {
+        flex: 1,
+    },
+
+    qrHeaderText: {
+        fontSize: 30,
+        fontWeight: 800,
+    },
+
+    qrHeader: {
+        height: 80,
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 16,
+        justifyContent: 'center'
+    },
+
+    qrCode: {
+        alignItems: "center",
+        padding: 16,
+        justifyContent: 'center'
+    },
+
+    qrBody: {
+        padding: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    qrText: {
+        textAlign: 'center'
+    }
 });
