01c99f8fbb91585d760c886b9427345f744d893f
LeoonKameleon <kacperwachala0@gmail.com>
2026-01-11 09:15:45 +0100

    points sync

--- src/frontend/screens/SpendPointsScreen.js
+++ src/frontend/screens/SpendPointsScreen.js
@@ -1,13 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { View, Text, FlatList} from 'react-native';
 import { styles } from '../styles/styles.js';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import OfferBody from '../components/offers/OfferBody.js';
 import ConfirmationModal from '../components/offers/ConfirmationModal.js';
+import { UserContext } from '../contexts/userContext.js';
 
 export default function SpendPointsScreen({ businessId, businessName }) {
     const [modalVisible, setModalVisible] = useState(false);
     const [offers, setOffers] = useState([]);
+    const { userID } = useContext(UserContext);
+    const [userPoints, setUserPoints] = useState(0);
     
         useEffect(() => {
             const fetchOffers = async () => {
@@ -22,6 +25,13 @@ export default function SpendPointsScreen({ businessId, businessName }) {
                     const filteredOffers = offersArray.filter(o => o.add_points === 0);
     
                     setOffers(filteredOffers);
+
+                    const pointsResp = await fetch(`http://192.168.0.9:5000/client-points/${userID}`);
+                    const pointsData = await pointsResp.json();
+                    const pointsForBusiness = pointsData[businessId] || 0;
+                    setUserPoints(pointsForBusiness);
+
+
                 } catch (err) {
                     console.error("Fetch offers error:", err);
                     Alert.alert("Błąd", "Nie udało się pobrać ofert");
@@ -43,7 +53,7 @@ export default function SpendPointsScreen({ businessId, businessName }) {
                     title="Potwierdź aktywację"
                     message="Czy na pewno chcesz aktywować wybraną ofertę?"
                 />
-                <Text style={styles.businessText}>Możesz wykorzystać punkty w {businessName}, wymieniając je na poniższe usługi. Obecnie masz x punktów.</Text>
+                <Text style={styles.businessText}>Możesz wykorzystać punkty w {businessName}, wymieniając je na poniższe usługi. Obecnie masz {userPoints} punktów.</Text>
                 <View style={styles.businessListContainer}>
                     <FlatList
                         style={styles.businessContainer}
